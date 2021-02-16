56860ea7c7f7e05d8d874cf04a7838f715c549c8
Radley <radleygh@gmail.com>
2021-02-16 13:05:36 -0800

    Added to git

---
+++ rs-jquery-browser.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: RS Jquery Browser Fix
+Description: Adds in the deprecated $.browser functionality to fix outdated plugins which still rely on that feature.
+Version: 1.0
+Author: Radley Sustaire
+Author URI: https://radleysustaire.com/
+*/
+
+function rsjq_enqueue() {
+	$url = untrailingslashit( plugin_dir_url( __FILE__ ) );
+	
+	wp_enqueue_script( 'jq-migrate', "$url/assets/jquery.browser.min.js", array('jquery'), '1.0');
+}
+add_action( 'wp_enqueue_scripts', 'rsjq_enqueue' );
